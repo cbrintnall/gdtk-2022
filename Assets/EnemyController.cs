3536fee9bb7962e985de777086cf4e15ba27f144
Jagi Natarajan <jagnat4000@gmail.com>
2022-07-16 01:51:19 -0700

    Make enemy not move into player (but player can still move into enemy)

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -10,6 +10,8 @@ public class EnemyController : MonoBehaviour
     LevelManager levelManager;
     EventManager eventManager;
 
+    Vector2Int TargetTile;
+
     void Start()
     {
         levelManager = FindObjectOfType<LevelManager>();
@@ -23,6 +25,12 @@ public class EnemyController : MonoBehaviour
     public void OnPlayerMove(PlayerMoveEvent e)
     {
         Debug.Log("Enemy got a player move event");
+        StartCoroutine(Utils.Defer(ActEnemy));
+    }
+
+    public void ActEnemy()
+    {
+        Debug.Log("MoveEnemy");
         Vector3 dir;
         if (IsPlayerVisible(out dir))
         {
@@ -30,9 +38,19 @@ public class EnemyController : MonoBehaviour
             Vector3 moveDir = dir.z < 0 ? new Vector3(0, 0, -1) : new Vector3(0, 0, +1);
             if (Mathf.Abs(dir.x) > Mathf.Abs(dir.z))
             {
-                moveDir =  dir.x < 0 ? new Vector3(-1, 0, 0) : new Vector3(+1, 0, 0);
+                moveDir = dir.x < 0 ? new Vector3(-1, 0, 0) : new Vector3(+1, 0, 0);
             }
             transform.forward = moveDir;
+
+            Vector2Int targetTile = GridMover.GetTargetTile();
+            Vector3Int playerTile = levelManager.Player.GetComponent<GridMovement>().CurrentTile;
+
+            if (targetTile == Utils.xy(playerTile))
+            {
+                // TODO: set up attack state here
+                return;
+            }
+
             GridMover.Move(1);
         }
     }
