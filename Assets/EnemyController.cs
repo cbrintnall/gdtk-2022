44002f60a0f2585088fca04dc9e5eb1d01543f0e
Christian Brintnall <christianbrintnall@gmail.com>
2022-07-15 22:16:56 -0700

    Update from plasticscm

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -23,7 +23,32 @@ public class EnemyController : MonoBehaviour
     public void OnPlayerMove(PlayerMoveEvent e)
     {
         Debug.Log("Enemy got a player move event");
-        GridMover.Move(1);
+        Vector3 dir;
+        if (IsPlayerVisible(out dir))
+        {
+            dir.y = 0;
+            Vector3 moveDir = dir.z < 0 ? new Vector3(0, 0, -1) : new Vector3(0, 0, +1);
+            if (Mathf.Abs(dir.x) > Mathf.Abs(dir.z))
+            {
+                moveDir =  dir.x < 0 ? new Vector3(-1, 0, 0) : new Vector3(+1, 0, 0);
+            }
+            transform.forward = moveDir;
+            GridMover.Move(1);
+        }
+    }
+
+    bool IsPlayerVisible(out Vector3 playerDirection)
+    {
+        var playerTransform = levelManager.Player.transform;
+        RaycastHit hitInfo;
+        var dirVec = playerTransform.position - transform.position;
+        playerDirection = dirVec.normalized;
+        Ray ray = new Ray(transform.position, dirVec);
+        if (Physics.Raycast(ray, out hitInfo, dirVec.magnitude))
+        {
+            return hitInfo.collider.CompareTag("Player");
+        }
+        return true;
     }
 
     void Update()
--- Assets/GridMovement.cs
+++ Assets/GridMovement.cs
@@ -2,42 +2,42 @@ using UnityEngine;
 
 public class GridMovement : MonoBehaviour
 {
-  public Grid Grid;
-  public Vector3Int CurrentTile => Grid.WorldToCell(transform.position);
+    public Grid Grid;
+    public Vector3Int CurrentTile => Grid.WorldToCell(transform.position);
 
-  private void Start()
-  {
-    Utils.AlignToGrid(Grid, transform);
-  }
+    private void Start()
+    {
+      Utils.AlignToGrid(Grid, transform);
+    }
 
-  public void Move(int tiles)
-  {
-    Utils.AlignToGrid(Grid, transform);
+    public void Move(int tiles)
+    {
+      Utils.AlignToGrid(Grid, transform);
 
-    Vector3 dir = new Vector3(
-      Grid.cellSize.x * transform.forward.x,
-      Grid.cellSize.y * transform.forward.y,
-      Grid.cellSize.z * transform.forward.z
-    );
+      Vector3 dir = new Vector3(
+        Grid.cellSize.x * transform.forward.x,
+        Grid.cellSize.y * transform.forward.y,
+        Grid.cellSize.z * transform.forward.z
+      );
 
-    Vector3 dest = dir * tiles;
-    transform.position += new Vector3(dest.x, 0f, dest.z);
-  }
+      Vector3 dest = dir * tiles;
+      transform.position += new Vector3(dest.x, 0f, dest.z);
+    }
 
-  public void Rotate(bool right)
-  {
-    float rotationAmt = right ? 90f : -90f;
+    public void Rotate(bool right)
+    {
+        float rotationAmt = right ? 90f : -90f;
 
-    transform.rotation = Quaternion.Euler(
-      transform.rotation.eulerAngles.x,
-      transform.rotation.eulerAngles.y + rotationAmt,
-      transform.rotation.eulerAngles.z
-    );
-  }
+        transform.rotation = Quaternion.Euler(
+          transform.rotation.eulerAngles.x,
+          transform.rotation.eulerAngles.y + rotationAmt,
+          transform.rotation.eulerAngles.z
+        );
+    }
 
-  private void OnDrawGizmosSelected()
-  {
-    Gizmos.color = Color.cyan;
-    Gizmos.DrawRay(transform.position, transform.forward);
-  }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawRay(transform.position, transform.forward);
+    }
 }
