42b3f4bb0333da09cf0a93a3db53bd19ade66a59
Christian Brintnall <christianbrintnall@gmail.com>
2022-07-16 20:34:34 -0700

    finish basic dice movement

--- Assets/DiceMovement.cs
+++ Assets/DiceMovement.cs
@@ -2,32 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Arc.Lib.Debug;
 
-[ExecuteInEditMode]
 public class DiceMovement : MonoBehaviour
 {
   public Camera Overlay;
   public CinemachineVirtualCamera Main;
 
-  // TODO: These should be cached on a settings changed event or something
-  public int ScreenWidth;
-  public int ScreenHeight;
-
+  public LayerMask DiceMouseCastLayer;
   public Vector3 Anchor;
+  public float ZOffset = 1f;
 
   // If the dice is attached to the player or being moved into world space
-  bool attachedToPlayer = true;
-
+  public bool attachedToPlayer = true;
+  DebugManager dbg;
+  Vector3 lastCastPosition;
+  ConstantRotation rotation;
+  
   private void Awake()
   {
+    rotation = GetComponent<ConstantRotation>();
+    dbg = FindObjectOfType<DebugManager>();
     attachedToPlayer = true;
   }
 
   private void Update()
   {
+    rotation.enabled = attachedToPlayer;
+
     if (attachedToPlayer)
     {
       transform.position = Overlay.ViewportToWorldPoint(Anchor);
+
+      var ray = Overlay.ScreenPointToRay(Input.mousePosition);
+
+      string diceHitName = "";
+
+      if (Physics.Raycast(ray, out RaycastHit hit, float.PositiveInfinity, DiceMouseCastLayer))
+      {
+        diceHitName = hit.collider.name;
+        if (Input.GetMouseButtonDown(0))
+          attachedToPlayer = false;
+      }
+
+      dbg.Track("Dice hit", diceHitName);
     }
+    else
+    {
+      Vector3 mouseTranslationVec = new Vector3(Input.mousePosition.x, Input.mousePosition.y, ZOffset);
+
+      dbg.Track("Mouse position", mouseTranslationVec);
+
+      Vector3 targetPosition = Overlay.ScreenToWorldPoint(mouseTranslationVec);
+
+      transform.position = targetPosition;
+
+      dbg.Track("Dice mouse position", targetPosition);
+    }
+
+    if (!Input.GetMouseButton(0))
+      attachedToPlayer = true;
+  }
+
+  private void OnDrawGizmosSelected()
+  {
+    Gizmos.color = Color.blue;
+    Gizmos.DrawSphere(lastCastPosition, .25f);
   }
 }
